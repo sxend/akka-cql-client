247e3014f99e4bd3e64bc99e959fe5fd3afdd83b
sxend <arimitsu.k+github@gmail.com>
2014-06-11 23:19:46 +0900

    add rows class

---
+++ src/main/scala/arimitsu/sf/cql/v3/messages/Metadata.java
@@ -0,0 +1,77 @@
+package arimitsu.sf.cql.v3.messages;
+
+import arimitsu.sf.cql.v3.util.Notation;
+
+import java.nio.ByteBuffer;
+import java.util.*;
+
+/**
+ * Created by sxend on 14/06/11.
+ */
+public class Metadata {
+    // <flags><columns_count>[<paging_state>][<global_table_spec>?<col_spec_1>...<col_spec_n>]
+    public final int flags;
+    public final int columnsCount;
+    public final byte[] pagingState;
+    public final Set<Object> globalTableSpec;
+
+    public Metadata(int flags, int columnsCount, byte[] pagingState, Set<Object> globalTableSpec) {
+        this.flags = flags;
+        this.columnsCount = columnsCount;
+        this.pagingState = pagingState;
+        this.globalTableSpec = globalTableSpec;
+    }
+
+
+    public static enum MetadataFlags {
+        GLOBAL_TABLES_SPEC(0x0001),
+        HAS_MORE_PAGES(0x0002),
+        NO_METADATA(0x0004);
+
+        public final int mask;
+
+        MetadataFlags(int mask) {
+            this.mask = mask;
+        }
+
+        public static MetadataFlags valueOf(int mask) {
+            for (MetadataFlags f : values()) {
+                if (f.mask == mask) return f;
+            }
+            throw new RuntimeException("invalid mask");
+        }
+    }
+
+    public static class MetadataParser {
+        public Metadata parse(ByteBuffer buffer) {
+            int flags = buffer.getInt();
+            int count = buffer.getInt();
+            boolean hasPagingState = (MetadataFlags.HAS_MORE_PAGES.mask & flags) > 0;
+            boolean hasGlobalSetting = (MetadataFlags.GLOBAL_TABLES_SPEC.mask & flags) > 0;
+            byte[] pagingState = null;
+            if (hasPagingState) {
+                pagingState = Notation.getBytes(buffer);
+            }
+            Set<Object> set = new HashSet<>();
+            for (int i = 0; i < count; i++) {
+                String globalSetting = null;
+                if(hasGlobalSetting){
+                    globalSetting = Notation.getString(buffer) + Notation.getString(buffer);
+                    set.add(globalSetting);
+                }
+                String keySpace = null;
+                String table = null;
+                if(!hasGlobalSetting){
+                    keySpace = Notation.getString(buffer);
+                    table = Notation.getString(buffer);
+                    set.add(keySpace+table);
+                }
+                String cname = Notation.getString(buffer);
+                Notation.OptionNotation option = Notation.getOption(buffer);
+                set.add(cname);
+                set.add(option);
+            }
+            return new Metadata(flags,count,pagingState,set);
+        }
+    }
+}
--- src/main/scala/arimitsu/sf/cql/v3/messages/results/Rows.java
+++ src/main/scala/arimitsu/sf/cql/v3/messages/results/Rows.java
@@ -1,6 +1,8 @@
 package arimitsu.sf.cql.v3.messages.results;
 
+import arimitsu.sf.cql.v3.messages.Metadata;
 import arimitsu.sf.cql.v3.messages.Result;
+import arimitsu.sf.cql.v3.util.Notation;
 
 import java.nio.ByteBuffer;
 
@@ -9,15 +11,33 @@ import java.nio.ByteBuffer;
  */
 public class Rows implements Result {
     // <metadata><rows_count><rows_content>
+    public final Object metadata;
+    public final Object rowsCount;
+    public final Object rowsContent;
+
+    public Rows(Object metadata, Object rowsCount, Object rowsContent) {
+        this.metadata = metadata;
+        this.rowsCount = rowsCount;
+        this.rowsContent = rowsContent;
+    }
+
     @Override
     public Kind getKind() {
         return Kind.ROWS;
     }
 
+    private static final Metadata.MetadataParser metadataParser= new Metadata.MetadataParser();
     public static final ResultParser<Rows> PARSER = new ResultParser<Rows>() {
         @Override
         public Rows parse(ByteBuffer body) {
-            return new Rows();
+            Metadata metadata = metadataParser.parse(body);
+            int rowsCount =body.getInt();
+            for(int i =0;i<rowsCount;i++){
+                for(int j =0;j< metadata.columnsCount;j++){
+                    Notation.getBytes(body);
+                }
+            }
+            return null;
         }
     };
 }
