b4276e7bc764a3cc85a66e05467bab52b881ee3e
sxend <arimitsu.k+github@gmail.com>
2014-06-17 20:02:36 +0900

    impled int type parser

--- src/main/scala/arimitsu/sf/cql/v3/messages/results/columntype/IntType.java
+++ src/main/scala/arimitsu/sf/cql/v3/messages/results/columntype/IntType.java
@@ -4,14 +4,22 @@ import arimitsu.sf.cql.v3.messages.ColumnType;
 import arimitsu.sf.cql.v3.messages.ColumnTypeEnum;
 import arimitsu.sf.cql.v3.messages.Parser;
 
+import java.nio.ByteBuffer;
+
 public class IntType implements ColumnType {
+    private static final Parser<Integer> PARSER = new Parser<Integer>() {
+        @Override
+        public Integer parse(ByteBuffer buffer) {
+            return buffer.getInt();
+        }
+    };
     @Override
     public short getId() {
         return ColumnTypeEnum.INT.id;
     }
 
     @Override
-    public Parser<?> getParser() {
-        return null;
+    public Parser<Integer> getParser() {
+        return PARSER;
     }
 }
