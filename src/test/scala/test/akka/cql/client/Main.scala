8764b57f6ce72baddcff2ca80ce8d28da8828b6f
sxend <arimitsu.k+github@gmail.com>
2014-07-06 16:44:31 +0900

    refactor test code

--- src/test/scala/test/akka/cql/client/Main.scala
+++ src/test/scala/test/akka/cql/client/Main.scala
@@ -4,11 +4,12 @@ import akka.actor.ActorSystem
 import java.net.InetSocketAddress
 import arimitsu.sf.akka.cqlclient.Cluster
 import arimitsu.sf.cql.v3.{Compression, Flags}
-import arimitsu.sf.cql.v3.messages.{Rows, Result, Query, QueryParameters}
+import arimitsu.sf.cql.v3.messages.{Rows, Result, QueryParameters}
+import arimitsu.sf.cql.v3.messages.Query
 import arimitsu.sf.cql.v3.messages.QueryParameters.ListValues
 import arimitsu.sf.cql.v3.Consistency._
 import arimitsu.sf.akka.cqlclient.Configuration
-import scala.util.{Try, Success, Failure}
+import scala.util.{Success, Failure}
 import scala.collection.JavaConversions._
 
 /**
@@ -42,10 +43,10 @@ object Main {
           r =>
             println(System.currentTimeMillis())
             client.startup()
-        }.flatMap{
+        }.flatMap {
           r =>
             val queryParam = new QueryParameters(LOCAL_ONE, Query.QueryFlags.VALUES.mask, new ListValues(), 1, new Array[Byte](1), ALL, System.currentTimeMillis())
-            client.query("use test",queryParam)
+            client.query("use test", queryParam)
         }.flatMap {
           r =>
             println(System.currentTimeMillis())
@@ -66,26 +67,34 @@ object Main {
                   }
                 }
                 val prepareFuture = client.prepare("select * from test_table1 where id = ?")
-                prepareFuture.map{
+                prepareFuture.map {
                   pre => {
-                    val rowPrint:Try[Result] => Unit = {
-                      case Success(s) if s.isInstanceOf[Rows] =>
-                        s.asInstanceOf[Rows].rowsContent.foreach {
-                          row => row.foreach {
-                            cols => println(cols.name + " -> " + cols.value)
+                    def resultPrint(result: Result) = {
+                      result match {
+                        case rows: Rows =>
+                          println("row print!!")
+                          rows.rowsContent.foreach {
+                            row => row.foreach {
+                              cols => println(cols.name + " -> " + cols.value)
+                            }
                           }
-                        }
-                      case Success(s) => println(s)
-                      case Failure(t) => t.printStackTrace()
+                        case _ =>
+                      }
                     }
                     val listval1 = new ListValues()
                     listval1.putInt(1)
                     val param1 = new QueryParameters(LOCAL_ONE, Query.QueryFlags.VALUES.mask, listval1, 1, new Array[Byte](1), ALL, System.currentTimeMillis())
-                    client.execute(pre.id,param1).onComplete(rowPrint)
                     val listval2 = new ListValues()
                     listval2.putInt(2)
                     val param2 = new QueryParameters(LOCAL_ONE, Query.QueryFlags.VALUES.mask, listval2, 1, new Array[Byte](1), ALL, System.currentTimeMillis())
-                    client.execute(pre.id,param2).onComplete(rowPrint)
+                    val result = for {
+                      result1 <- client.execute(pre.id, param1)
+                      result2 <- client.execute(pre.id, param2)
+                    } yield Array(result1, result2)
+                    result.onComplete {
+                      case Success(s) => s.foreach(resultPrint)
+                      case Failure(t) => t.printStackTrace()
+                    }
                   }
                 }
               case _ =>
