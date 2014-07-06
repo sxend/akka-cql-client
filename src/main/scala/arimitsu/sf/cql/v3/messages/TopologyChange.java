1516eaf6dd8e904b0d2a10b1f5caf81d9e6fbf03
sxend <arimitsu.k+github@gmail.com>
2014-07-06 15:27:13 +0900

    refactor topology change event

--- src/main/scala/arimitsu/sf/cql/v3/messages/TopologyChange.java
+++ src/main/scala/arimitsu/sf/cql/v3/messages/TopologyChange.java
@@ -1,17 +1,35 @@
 package arimitsu.sf.cql.v3.messages;
 
+import arimitsu.sf.cql.v3.util.Notation;
+
+import java.net.InetAddress;
 import java.nio.ByteBuffer;
 
 /**
  * Created by sxend on 14/07/06.
  */
 public class TopologyChange implements Event {
+    public TopologyChange(ChangeType changeType, InetAddress nodeAddress) {
+        this.changeType = changeType;
+        this.nodeAddress = nodeAddress;
+    }
+
+    public static enum ChangeType {
+        NEW_NODE,
+        REMOVED_NODE,;
+    }
+
+    public final ChangeType changeType;
+    public final InetAddress nodeAddress;
+
     @Override
     public EventType getType() {
         return EventType.TOPOLOGY_CHANGE;
     }
 
     public static TopologyChange fromBuffer(ByteBuffer buffer) {
-        return null;
+        ChangeType changeType = ChangeType.valueOf(Notation.getString(buffer));
+        InetAddress nodeAddress = Notation.getINet(buffer);
+        return new TopologyChange(changeType, nodeAddress);
     }
 }
